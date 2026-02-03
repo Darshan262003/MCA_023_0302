e905ae53f8afa4717c308d653bad391bce42151c
Charitha1705 <1ms24mc020@gmail.com>
2026-02-03 09:50:55 +0530

    exam commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <p>LOGIN PAGE</p>
+     <input type="text" placeholder='Enter Name'></input>
+     <input type="text" placeholder='Password'></input>
+     <button>LOGIN</button>
     </div>
   );
 }
